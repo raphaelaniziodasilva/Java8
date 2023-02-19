eeb7fbd28193d7abf5288bae75ddb83a59ee6790
Raphael Anizio <raphaelanizio01@gmail.com>
2023-02-19 11:38:55 -0300

    usando o lambdas -> e parecido com as arrow function => usam a mesma forma

--- src/OdenaSings.java
+++ src/OdenaSings.java
@@ -12,63 +12,20 @@ public class OdenaSings {
         palavras.add("Bayer");
         palavras.add("City");
 
-        // fazendo a ordenação da nossa lista por tamanho da string para isso vamos usar a interface Comparator
-        // precisamos criar uma classe e implementar a interface Comparator
-
-        // criando o comparador de String que vai ser do tipo ComparadorPorTamanho
-        Comparator<String> comparador = new ComparadorPorTamanho();
-
-        // fazendo a ordenação pelo tamanho da String
-        palavras.sort(comparador);
+        // fazendo a ordenação da nossa lista por tamanho da string do menor para o maior
+        // vamos usar o metodo sort do java para fazer a ordenação da nossa lista
+        // o lambdas -> e parecido com as arrow function => usam a mesma forma
+        palavras.sort((s1, s2) -> {
+            // utilizando metodo compare do java para comparar duas strings uma com a outra
+            return Integer.compare(s1.length(), s2.length());
+        });
         System.out.println(palavras);
 
-        // vamos imprimir todos elements da lista e para isso utilizamos o (for) para percorrer o array e imprimir cada elmento
-        /**
-         *  for (String p: palavras) {
-         *             System.out.println(p);
-         *      }
-         */
-
-        // vamos fazer uma outra forma de percorrer a lista e imprimir seus elementos e para isso vamos utilizar a Consumer
-        // precisamos criar uma classe e implementar a interface Consumer
-
-        // criando o consumidor de String que vai ser do tipo ImprimeNaLinha
-        Consumer<String> consumidor = new ImprimeNaLinha();
-        // o forEach vai receber um consumidor de Strings e vai imprimir a lista
-        palavras.forEach(consumidor);
+        // vamos utilizar o forEach percorrer a lista e imprimir seus elementos
+        // o lambdas -> e parecido com as arrow function => usam a mesma forma
+        palavras.forEach(s -> {
+            // dentro do array de palavras para cada palavra no indice s imprima o indice s
+            System.out.println(s);
+        });
     }
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-    // Comparator e responsavel por você implementar e poder dizer olha essa string aqui vem antes e essa depois etc...
-    // assim podemos ordenar a lista de string por um outro criterio do jeito que agente preferir
-
-    @Override
-    // vamos reescrever esse metodo compare do java.util para poder fazer a ordenação pelo tamanho de String
-    // vamos pegar 2 strings e compara uma com a outra pelo tamanho dela
-    // se a primeira string for vir antes da segunda string deve retornar um numero negativo se ele vier depois da
-    // segunda string ele retorna um valor positivo e se der um empate retorna 0
-    public int compare(String s1, String s2) {
-        if(s1.length() < s2.length()) {
-            return -1;
-        }
-        if(s1.length() > s2.length()) {
-            return 1;
-        }
-        return 0;
-    }
-    // agora vamos voltar para a classe OdenaSings e criar o Comparator de string
-}
-
-// criando a classe e implementando a nova interface do Java 8 Consumer
-class ImprimeNaLinha implements Consumer<String> {
-
-    // reescrevendo o metodo accept
-    @Override
-    // vai funcionar como se fosse o for passando por cada elemento do array
-    public void accept(String s) {
-        // toda vez que eu for consumir uma string eu vou
-        System.out.println(s); // imprimir a string
-    }
-    // agora vamos voltar para a classe OdenaSings e criar o Consumer de string
 }
