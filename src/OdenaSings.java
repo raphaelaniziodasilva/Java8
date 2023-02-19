bfe5e7361ffaf9371c84e8eccbbb776ee353b086
Raphael Anizio <raphaelanizio01@gmail.com>
2023-02-19 12:10:13 -0300

    usando o lambdas -> e parecido com as arrow function => usam a mesma forma

--- src/OdenaSings.java
+++ src/OdenaSings.java
@@ -18,6 +18,7 @@ public class OdenaSings {
 
         // primeira forma utilizando o lambda
         // fazendo a ordenação da lista utilizando o metodo sort do java e comparando duas strings uma com a outra
+
         palavras.sort((s1, s2) -> {
             if(s1.length() < s2.length())
                 return -1;
@@ -25,6 +26,7 @@ public class OdenaSings {
                 return 1;
             return 0;
         });
+        System.out.println(palavras);
 
         // segunda forma utilizando o lambda junto com o metodo compare
         // fazendo a ordenação da lista utilizando o metodo sort do java
@@ -34,11 +36,27 @@ public class OdenaSings {
         });
         System.out.println(palavras);
 
+        // terceira forma utilizando o lambda junto com o metodo Comparator.coparing
+        // fazendo a ordenação da lista utilizando o metodo sort do java
+        palavras.sort(Comparator.comparing(s -> s.length()));
+        System.out.println(palavras);
+
+        // quarta forma utilizando o metodo reference :: que é so uma forma de escrever o lambda de uma maneira mais enxuta
+        // o metodo reference so serve quando for invocar um unico metodo
+        palavras.sort(Comparator.comparing(String::length));
+        System.out.println(palavras);
+
+
         // vamos utilizar o forEach percorrer a lista e imprimir seus elementos
         // o lambdas -> e parecido com as arrow function => usam a mesma forma
         palavras.forEach(s -> {
             // dentro do array de palavras para cada palavra no indice s imprima o indice s
             System.out.println(s);
         });
+
+        // utilizando o metodo reference :: que é so uma forma de escrever o lambda de uma maneira mais enxuta
+        // o metodo reference so serve quando for invocar um unico metodo
+        // vamos imprimir os elementos da lista utilizando o forEach e reference
+        palavras.forEach(System.out::println);
     }
 }
